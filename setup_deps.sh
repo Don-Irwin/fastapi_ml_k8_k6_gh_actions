82a5f60f35eb55ccf9847ca724ee4500d421363c
Don-Irwin <148596325+Don-Irwin@users.noreply.github.com>
2023-10-26 02:21:20 -0400

    Update setup_deps.sh

--- setup_deps.sh
+++ setup_deps.sh
@@ -1,4 +1,4 @@
-    #!/bin/bash
+#!/bin/bash
 
     username="ubuntu"
 
@@ -10,7 +10,7 @@
         echo "User $username has been created with no password."
     fi
 
-    apt update 
+    apt update
 
     #remove prompting on auto builds
     apt-get remove needrestart -y
@@ -19,7 +19,7 @@
     apt install docker -y && apt install docker-compose -y && apt install git -y && apt install python-is-python3 -y && apt install nano -y
 
     #get poetry
-    curl -sSL https://install.python-poetry.org | POETRY_HOME=/ python -    
+    curl -sSL https://install.python-poetry.org | POETRY_HOME=/ python -
 
     #get minikube
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
@@ -29,29 +29,24 @@
     snap install k6
 
     #istio
-    sudo -u ubuntu curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.19.0 TARGET_ARCH=x86_64 sh -
-    sudo -u ubuntu chmod +x  istio-1.19.0
-    sudo -u ubuntu mkdir -p ~/.local/bin
-    sudo -u ubuntu mv ./istio-1.19.0 ~/.local/bin/istio-1.19.0
-    echo "PATH=$PATH:~/.local/bin/istio-1.19.0/bin">>~/.bashrc
-    sudo -u ubuntu source ~/.bashrc
-
-
-    #get into our directory
-    cd /home/ubuntu/
-    #/home/ubuntu/w255_deployers/lab3_k8
-
-    usermod -aG docker ubuntu
-    newgrp docker
-
+    #istio
+    curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.19.0 TARGET_ARCH=x86_64 sh -
+    chmod 777 -R  istio-1.19.0
+    rm -rf /usr/local/bin/istio-1.19.0
+    mv ./istio-1.19.0 /usr/local/bin/istio-1.19.0
+    echo "PATH=$PATH:/usr/local/bin/istio-1.19.0/bin/">>/home/ubuntu/.bashrc
+    export PATH="$PATH:/usr/local/bin/istio-1.19.0/bin/"
+    echo "PATH=\"$PATH\"">/etc/environment
 
     curl -LO https://dl.k8s.io/release/v1.28.1/bin/linux/amd64/kubectl
     install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 
     #make this last
-    apt install nginx -y    
+    apt install nginx -y
 
     #allow other processes to write to directory
     chmod -R 777 /var/www/html/
 
+    usermod -aG docker ubuntu
+    newgrp docker
